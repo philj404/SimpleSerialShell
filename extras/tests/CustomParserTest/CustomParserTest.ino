b8029df6b154a4013c0b4d46a0ddbed64b1b9a72
Bruce MacKinnon <bruce@mackinnon.com>
2021-12-29 14:00:37 -0500

    Refactored the unit test per Phil's original request

--- extras/tests/CustomParserTest/CustomParserTest.ino
+++ extras/tests/CustomParserTest/CustomParserTest.ino
@@ -16,7 +16,8 @@
 
 using namespace aunit;
 
-SimMonitor terminal;
+// A mock of the Arduino Serial stream
+static SimMonitor terminal;
 
 void prepForTests(void)
 {
@@ -41,26 +42,20 @@ class ShellTest: public TestOnce {
 };
 
 //////////////////////////////////////////////////////////////////////////////
-// A demonstration of the alternate tokenizer feature.  Please
-// note that this test runs with an independent shell and an 
-// independent terminal in order to avoid any conflict with global
-// state that appears elsewhere in this library.
-//
-static SimpleSerialShell shell2;
-static SimMonitor terminal2;
+// A demonstration of the alternate tokenizer feature. 
 
 /**
  * @brief Echoes the parameters of the function with comma-delimiting.
  * @return int 0 On success, non-zero on error.
  */
-int echo2(int argc, char **argv) {
+int echo(int argc, char **argv) {
     auto lastArg = argc - 1;
     for ( int i = 1; i < argc; i++) {
-        shell2.print(argv[i]);
+        shell.print(argv[i]);
         if (i < lastArg)
-            shell2.print(F(" "));
+            shell.print(F(" "));
     }
-    shell2.println();
+    shell.println();
     return 0;
 }
 
@@ -81,31 +76,31 @@ char* commaTokenizer(char* str, const char*, char** saveptr) {
 
 testF(ShellTest, altTokenizer) {
 
-    const char* testCommand = "echo2 test1,test2,test3";
+    const char* testCommand = "echo test1,test2,test3";
 
     // Basic sanity check using the current semantics
-    terminal2.pressKeys(testCommand);
-    assertFalse(shell2.executeIfInput());
+    terminal.pressKeys(testCommand);
+    assertFalse(shell.executeIfInput());
     // Flush the user-entry
-    terminal2.getline();
-    terminal2.pressKey('\r');
-    assertTrue(shell2.executeIfInput());
+    terminal.getline();
+    terminal.pressKey('\r');
+    assertTrue(shell.executeIfInput());
     // Everything comes back as a single token
-    assertEqual(terminal2.getline(), "\r\ntest1,test2,test3\r\n");
+    assertEqual(terminal.getline(), "\r\ntest1,test2,test3\r\n");
 
     // Now plug in a new tokenizer that looks for commas instead of spaces.
     // This is the main point of this unit test.
-    shell2.setTokenizer(commaTokenizer);
+    shell.setTokenizer(commaTokenizer);
 
     // Send in the same command
-    terminal2.pressKeys(testCommand);
-    assertFalse(shell2.executeIfInput());
+    terminal.pressKeys(testCommand);
+    assertFalse(shell.executeIfInput());
     // Flush the user-entry
-    terminal2.getline();
-    terminal2.pressKey('\r');
-    assertTrue(shell2.executeIfInput());
+    terminal.getline();
+    terminal.pressKey('\r');
+    assertTrue(shell.executeIfInput());
     // Notice now that the three tokens were separated
-    assertEqual(terminal2.getline(), "\r\ntest1 test2 test3\r\n");
+    assertEqual(terminal.getline(), "\r\ntest1 test2 test3\r\n");
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -114,12 +109,8 @@ void setup() {
     Serial.begin(115200); // ESP8266 default of 74880 not supported on Linux
     while (!Serial); // for the Arduino Leonardo/Micro only
 
-    // NB: Even though this command be being added to the shell2 instance, 
-    // the list of commands is held in a static list so it will be visible
-    // in the shell instance as well.  This is the reason why we are 
-    // calling the command "echo2."
-    shell2.addCommand(F("echo2"), echo2);
-    shell2.attach(terminal2);
+    shell.addCommand(F("echo"), echo);
+    shell.attach(terminal);
 }
 
 //////////////////////////////////////////////////////////////////////////////
